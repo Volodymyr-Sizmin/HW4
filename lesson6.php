4fe5336654e5b98ff57e5850daf958e30cee8dae
Volodymyr Sizmin <ukrmedias5@gmail.com>
2022-08-09 11:56:36 +0300

    Composer instal

--- lesson6.php
+++ lesson6.php
@@ -1,13 +1,19 @@
 <?php
-require_once "classes\Currency.php";
-require_once "classes\Money.php";
+require_once __DIR__ . '/vendor/autoload.php';
+//spl_autoload_register(function ($className){
+//    $file = __DIR__.'/'.str_replace('\\', '/', $className).'.php';
+//    if (!file_exists($file)){
+//        throw new \InvalidArgumentException("Class [{$className}] doesn't exist in [{$file}] path");
+//    }
+//    require_once  $file;
+//});
 
 use classes\Currency;
 use classes\Money;
 
 
 try {
-    $money = new Money("USD", 350);
+    $money = new Money("UAH", 350);
     $newMoney = new Money("USD", 250);
     if ($money->equals($newMoney)){
         echo "Objects are equal!" . "<br>";
