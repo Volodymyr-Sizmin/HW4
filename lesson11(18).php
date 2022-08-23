68059b7eefe1f67ab488192ffd896997d85fc2ba
Volodymyr Sizmin <ukrmedias5@gmail.com>
2022-08-23 14:45:25 +0300

    Dependensy inversion

---
+++ lesson11(18).php
@@ -0,0 +1,29 @@
+<?php
+
+interface Database
+{
+    public function getData();
+}
+
+class Mysql implements  Database
+{
+    public function getData()
+    {
+        return 'some data from database';
+    }
+}
+
+class Controller
+{
+    private Database $adapter;
+
+    public function __construct(Database $mysql)
+    {
+        $this->adapter = $mysql;
+    }
+
+    function getData()
+    {
+        $this->adapter->getData($this);
+    }
+}
